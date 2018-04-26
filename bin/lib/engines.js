b7fe49508a38f6e24091f15d345a56e09dc92b01
Stéphane Zucatti <stephane.zucatti@me.com>
2018-04-26 11:30:17 +0200

    UPD: support MariaDB, drop MySQL

--- bin/lib/engines.js
+++ bin/lib/engines.js
@@ -1,4 +1,4 @@
-module.exports = function(Config, cb) {
+module.exports = function (Config, cb) {
 
     var path = require('path');
     var fs = require('fs');
@@ -6,7 +6,7 @@ module.exports = function(Config, cb) {
 
     function makedirs(dirs, i, cb) {
         if (!dirs[i]) return cb();
-        fs.mkdir(dirs[i], function() {
+        fs.mkdir(dirs[i], function () {
             makedirs(dirs, i + 1, cb);
         });
     };
@@ -24,20 +24,31 @@ module.exports = function(Config, cb) {
         __dirname + path.sep + '..' + path.sep + '..' + path.sep + 'config' + path.sep + "engines" + path.sep + "nginx" + path.sep + "production"
     ];
 
-    fs.readFile(__dirname + path.sep + 'nginx.tpl', function(e, b) {
-        makedirs(DIRS, 0, function() {
+    fs.readFile(__dirname + path.sep + 'nginx.tpl', function (e, b) {
+        makedirs(DIRS, 0, function () {
 
-            console.log('   - Starting MySQL process...');
+            console.log('   - Starting MariaDB process...');
 
             var exec = require('child_process').execFile;
+            var spawn = require('child_process').spawn;
 
-            var dmysql = exec(__dirname + path.sep + '..' + path.sep + 'mysql' + path.sep + 'bin' + path.sep + 'mysqld', [
+            /*var dmysql = exec(__dirname + path.sep + '..' + path.sep + 'mysql' + path.sep + 'bin' + path.sep + 'mysqld', [
                 "--defaults-file=" + __dirname + path.sep + '..' + path.sep + '..' + path.sep + 'config' + path.sep + "engines" + path.sep + "mysql" + path.sep + "my.cnf",
                 "-b", __dirname + path.sep + ".." + path.sep + "mysql",
                 "--datadir=" + __dirname + path.sep + ".." + path.sep + ".." + path.sep + "var" + path.sep + "db" + path.sep,
                 "--user=root",
                 "--port=" + Config["db.port"],
                 "--daemonize"
+            ]);
+            -- this is MySQL
+            */
+
+            var dmysql = spawn(__dirname + path.sep + '..' + path.sep + 'mysql' + path.sep + 'bin' + path.sep + 'mysqld', [
+                "--defaults-extra-file=" + __dirname + path.sep + '..' + path.sep + '..' + path.sep + 'config' + path.sep + "engines" + path.sep + "mysql" + path.sep + "my.cnf",
+                "-b", __dirname + path.sep + ".." + path.sep + "mysql",
+                "--datadir=" + __dirname + path.sep + ".." + path.sep + ".." + path.sep + "var" + path.sep + "db" + path.sep,
+                "--user=root",
+                "--port=" + Config["db.port"]
             ]);
 
             console.log('   - Starting MongoD process.');
@@ -57,19 +68,19 @@ module.exports = function(Config, cb) {
 
             console.log('   - Starting NginX process.');
 
-            fs.writeFile(__dirname + path.sep + '..' + path.sep + '..' + path.sep + 'config' + path.sep + "engines" + path.sep + "nginx" + path.sep + "nginx.conf", b.toString('utf-8'), function() {
+            fs.writeFile(__dirname + path.sep + '..' + path.sep + '..' + path.sep + 'config' + path.sep + "engines" + path.sep + "nginx" + path.sep + "nginx.conf", b.toString('utf-8'), function () {
                 var dnginx = exec(__dirname + path.sep + '..' + path.sep + 'nginx' + path.sep + 'nginx', [
                     "-c", __dirname + path.sep + '..' + path.sep + '..' + path.sep + 'config' + path.sep + "engines" + path.sep + "nginx" + path.sep + "nginx.conf",
                     "-p", __dirname + path.sep + '..' + path.sep + '..' + path.sep
                 ]);
 
-                tcpPortUsed.waitUntilUsed(Config["db.port"] * 1, 500, 4000).then(function() {
+                tcpPortUsed.waitUntilUsed(Config["db.port"] * 1, 500, 4000).then(function () {
                     console.log('[ STARTED ] MySQL service');
-                    tcpPortUsed.waitUntilUsed(Config["session.port"] * 1).then(function() {
+                    tcpPortUsed.waitUntilUsed(Config["session.port"] * 1).then(function () {
                         console.log('[ STARTED ] MongoDB service');
-                        tcpPortUsed.waitUntilUsed(61208).then(function() {
+                        tcpPortUsed.waitUntilUsed(61208).then(function () {
                             console.log('[ STARTED ] Stats service');
-                            tcpPortUsed.waitUntilUsed(80).then(function() {
+                            tcpPortUsed.waitUntilUsed(80).then(function () {
                                 console.log('[ STARTED ] HTTP/S service');
                                 cb();
                             });
